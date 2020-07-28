eb485d492f7041c9a771fb63fc1448f0fdb04fa9
liaolinsheng <liaolinsheng@c3425c7d-a6a7-4d3b-a928-41d958518f27>
2020-07-28 13:18:23 +0000

    repair
    git-svn-id: svn://192.168.0.200/cloud_program/houtai/release/data_lead@11991 c3425c7d-a6a7-4d3b-a928-41d958518f27

--- database/migrations/2020_07_27_124536_create_byte_click_data_table.php
+++ database/migrations/2020_07_27_124536_create_byte_click_data_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateByteAdDataTable extends Migration
+class CreateByteClickDataTable extends Migration
 {
     /**
      * Run the migrations.
