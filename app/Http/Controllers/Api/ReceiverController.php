7b3c96e4fb9630f76e8bf8fb7baaba9448c61273
liaolinsheng <liaolinsheng@c3425c7d-a6a7-4d3b-a928-41d958518f27>
2020-07-31 12:06:40 +0000

    '字节'接受接口初稿
    git-svn-id: svn://192.168.0.200/cloud_program/houtai/release/data_lead@12014 c3425c7d-a6a7-4d3b-a928-41d958518f27

--- app/Http/Controllers/Api/ReceiverController.php
+++ app/Http/Controllers/Api/ReceiverController.php
@@ -7,10 +7,21 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\ByteClickData;
 use App\Http\Requests\Api\ByteShowData;
 
+use Log;
+
+use App\Logic\AppUsers;
+use App\Logic\AppUsersUid;
+use App\Logic\AppUsersFormat;
+
+use App\Logic\AppByteShowData;
+use App\Logic\AppByteClickData;
+
 class ReceiverController extends Controller
 {
     public function __construct() {
         $this->middleware( 'gameappid_check' );
+        //调试接口日志
+        Log::debug( static::class .': requestUri '. request()->fullUrl() );
     }
 
     /**
@@ -24,8 +35,22 @@ class ReceiverController extends Controller
         $req_data = $request->all();
         $valiRes = static::jsonValidate( new ByteClickData, $req_data, $valiStatus );
         if( !$valiStatus ) {
+            Log::debug( static::class .': valiFail', $valiRes );
             return \response()->json( $valiRes );
         }
+        //获取唯一ID
+        $unique_id = AppUsersUid::fromByteClickData( $req_data );
+        $req_data['unique_id'] = $unique_id;
+        Log::debug( static::class .': unique_id '. $unique_id );
+
+        //应用用户逻辑
+        $AppUsers = new AppUsers( $app_id );
+        $AppUsers->create( AppUsersFormat::fromByteClickData( $req_data ) );
+
+        //字节点击数据逻辑
+        $AppByteClickData = new AppByteClickData( $app_id );
+        $AppByteClickData->create( $req_data );
+    
 
         return \response()->json( static::jsonRes( ) );
     }
@@ -41,8 +66,18 @@ class ReceiverController extends Controller
         $req_data = $request->all();
         $valiRes = static::jsonValidate( new ByteShowData, $req_data, $valiStatus );
         if( !$valiStatus ) {
+            Log::debug( static::class .': valiFail ', $valiRes );
             return \response()->json( $valiRes );
         }
+
+        //获取唯一ID
+        $unique_id = AppUsersUid::fromByteClickData( $req_data );
+        $req_data['unique_id'] = $unique_id;
+        Log::debug( static::class .': unique_id '. $unique_id );
+
+        //字节展示数据逻辑
+        $AppByteShowData = new AppByteShowData( $app_id );
+        $AppByteShowData->create( $req_data );
 
         return \response()->json( static::jsonRes( ) );
     }
