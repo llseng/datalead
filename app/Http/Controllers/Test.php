525993cd9e2a8d2c5ef77f0a2c105d29802aa675
liaolinsheng <liaolinsheng@c3425c7d-a6a7-4d3b-a928-41d958518f27>
2020-07-31 12:06:49 +0000

    测试控制器
    git-svn-id: svn://192.168.0.200/cloud_program/houtai/release/data_lead@12016 c3425c7d-a6a7-4d3b-a928-41d958518f27

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -6,12 +6,11 @@ use Illuminate\Http\Request;
 
 use DB;
 use App\BaseModel;
+use App\Logic\AppUsers as AppUsersLogic;
 
 class Test extends Controller
 {
     public function index() {
-        
-        dump( DB::statement( "create table test_table like game_app" ) );
-        dump( DB::statement( "drop table test_table" ) );
+        return \response()->json( static::jsonRes(404) );
     }
 }
