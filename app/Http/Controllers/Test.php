9d9557583e97b685bb19ded4c63c75c6f02e1921
liaolinsheng <liaolinsheng@c3425c7d-a6a7-4d3b-a928-41d958518f27>
2020-08-01 10:19:21 +0000

    测试字节监测链接数据模
    git-svn-id: svn://192.168.0.200/cloud_program/houtai/release/data_lead@12031 c3425c7d-a6a7-4d3b-a928-41d958518f27

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use DB;
+use App\Logic;
 use App\BaseModel;
-use App\Logic\AppUsers as AppUsersLogic;
 
 class Test extends Controller
 {
     public function index() {
-        return \response()->json( static::jsonRes(404) );
+        $data = [
+            'byte_click' => Logic\AppByteClickData::getUrlQuery(),
+            'byte_show' => Logic\AppByteShowData::getUrlQuery(),
+        ];
+        return \response()->json( static::jsonRes(404, null, $data) );
     }
 }
