61b2234638ff321773adc34dc2c6a8f6ed1fb776
liaolinsheng <liaolinsheng@c3425c7d-a6a7-4d3b-a928-41d958518f27>
2020-08-06 13:14:58 +0000

    更新应用初始化接口
    git-svn-id: svn://192.168.0.200/cloud_program/houtai/release/data_lead@12084 c3425c7d-a6a7-4d3b-a928-41d958518f27

--- app/Http/Controllers/Api/ListenController.php
+++ app/Http/Controllers/Api/ListenController.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Controller;
 
 use Log;
 
+use App\Logic\AppInitData as AppInitDataL;
+use App\Logic\AppDataFilter as AppDataFilterL;
+
 use App\Http\Requests\Api\AppInitData;
 
 class ListenController extends Controller
@@ -22,9 +25,26 @@ class ListenController extends Controller
         $valiRes = static::jsonValidate( new AppInitData, $req_data, $valiStatus );
         if( !$valiStatus ) {
             Log::debug( static::class .': valiFail', $valiRes );
-            return \response()->json( $valiRes );
+            // return \response()->json( $valiRes );
+            $req_data = AppDataFilterL::filterKeys( $req_data, \array_keys( $valiRes['data'] ) );
         }
-     
+
+        $req_data['ua'] = $request->header( 'user-agent' );
+        $req_data['ip'] = $request->getClientIp( );
+        if( empty( $req_data['ts'] ) ) {
+            $request_time = $request->server( 'REQUEST_TIME_FLOAT' )?: time();
+            $req_data['ts'] = \round( $request_time * 1000 );
+        }
+
+        $filter_data = AppDataFilterL::filter( $req_data );
+
+        $init_id = AppInitDataL::InitId( $filter_data );
+        $filter_data['init_id'] = $init_id;
+        Log::debug( static::class .': init_id ' .$init_id );
+
+        $AppInitDataL = new AppInitDataL( $app_id );
+        $init_data_create_status = $AppInitDataL->create( $filter_data );
+
         return \response()->json( static::jsonRes( ) );
     }
 }
