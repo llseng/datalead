b8bd1a27b72ada155f114d74f83dba59f328836a
liaolinsheng <liaolinsheng@c3425c7d-a6a7-4d3b-a928-41d958518f27>
2020-08-20 11:14:18 +0000

    feat(route):总览页Line图表接口路由
    git-svn-id: svn://192.168.0.200/cloud_program/houtai/release/data_lead@12206 c3425c7d-a6a7-4d3b-a928-41d958518f27

--- routes/api.php
+++ routes/api.php
@@ -32,3 +32,13 @@ Route::namespace('Api')->prefix('receiver')->group( function () {
 Route::namespace('Api')->prefix('listen')->group( function () {
     Route::get( "/app_init/{app_id}", "ListenController@app_init" )->where('app_id', "\w+")->name( 'app_init' );
 } );
+
+/**
+ * 总览页图表数据api路由组
+ */
+Route::namespace('Api')->prefix('homeChart')->group( function () {
+    Route::post( "/byte_click/{app_id}", "HomeChartController@byte_click" )->where('app_id', "\w+")->name( 'home_chart_byte_click' );
+    Route::post( "/app_init/{app_id}", "HomeChartController@app_init" )->where('app_id', "\w+")->name( 'home_chart_app_init' );
+    Route::post( "/activation/{app_id}", "HomeChartController@activation" )->where('app_id', "\w+")->name( 'home_chart_activation' );
+    Route::post( "/active/{app_id}", "HomeChartController@active" )->where('app_id', "\w+")->name( 'home_chart_active' );
+} );
