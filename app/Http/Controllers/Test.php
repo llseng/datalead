6593d58a7eca1fb3d77ac1ea0f2c6d5303197955
llseng <1300904522@qq.com>
2020-09-10 18:58:41 +0800

    test:测试内容页表格模板分页能力

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -19,51 +19,42 @@ class Test extends Controller
     public function index(TestVali $request) {
         $Table = new LC\Table();
 
-        $pid_form = new LC\TableFormInput( '玩家ID', 'pid', \request()->get('pid') );
+        $pid_form = new LC\TableFormInput( '启动ID', 'init_id', \request()->get('init_id') );
         // $pid_form->inputType( "number" );
-        $age_form = new LC\TableFormInput( '玩家年龄', 'age', \request()->get('age') );
+        $age_form = new LC\TableFormInput( '启动IP', 'ip', \request()->get('ip') );
         // $age_form->inputType( "number" );
-        $date_form = new LC\TableFormInput( '注册时间', 'date', \request()->get('date') );
+        $date_form = new LC\TableFormInput( '日期', 'date', \request()->get('date') );
         $date_form->inputType( "date" );
-        $level_form = new LC\TableFormSelect( '玩家等级', 'level', \request()->get('level') );
-        $level_form->setOptions( [1,2,3,4,5,6,7,8,9] );
+        $level_form = new LC\TableFormSelect( '系统', 'os', \request()->get('os') );
+        $level_form->setOptions( [
+            1 => "Android",
+            2 => "IOS"
+        ] );
 
         $Table->setRows( [$pid_form, $age_form, $date_form, $level_form] );
 
         // $Table->noFormBlock();
 
-        $id_line = new LC\TableInfo( "#", "idd" );
-        $name_line = new LC\TableInfo( "名称", "name" );
-        $age_line = new LC\TableInfo( "年龄", "age" );
-        $age_line->setHandler( function( $list, $obj ) {
-            $str = "";
-            $str .= ( isset( $list[ $obj->getName() ] )? $list[ $obj->getName() ]: '未知' );
-            $str .= "岁";
-            return $str;
-        } );
-        $level_line = new LC\TableInfo( "等级", "level" );
-        $btns_line = new LC\TableBtns( "操作", "" );
+        $id_line = new LC\TableInfo( "#", "id" );
+        $initid_line = new LC\TableInfo( "INITID", "init_id" );
+        $name_line = new LC\TableInfo( "IP", "ip" );
+        $age_line = new LC\TableInfo( "日期", "create_date" );
+        $level_line = new LC\TableInfo( "时间", "create_time" );
+        $btns_line = new LC\TableBtns( "操作", "update" );
         $btns_line->pushBtn( "修改", "s", "success" );
 
-        $Table->setLines( [$id_line, $name_line, $age_line, $level_line, $btns_line] );
-
-        $id = 0;
-        $data = [
-            ['idd' => $id++, "name" => "name_". rand( 1000, 9999 ), "age" => rand( 10, 99 ), "level" => rand( 1, 10 ) ],
-            ['idd' => $id++, "name" => "name_". rand( 1000, 9999 ), "age" => rand( 10, 99 ), "level" => rand( 1, 10 ) ],
-            ['idd' => $id++, "name" => "name_". rand( 1000, 9999 ), "age" => rand( 10, 99 ), "level" => rand( 1, 10 ) ],
-            ['idd' => $id++, "name" => "name_". rand( 1000, 9999 ), "age" => rand( 10, 99 ), "level" => rand( 1, 10 ) ],
-            ['idd' => $id++, "name" => "name_". rand( 1000, 9999 ), "age" => rand( 10, 99 ), "level" => rand( 1, 10 ) ],
-            ['idd' => $id++, "name" => "name_". rand( 1000, 9999 ), "age" => rand( 10, 99 ), "level" => rand( 1, 10 ) ],
-            ['idd' => $id++, "name" => "name_". rand( 1000, 9999 ), "age" => rand( 10, 99 ), "level" => rand( 1, 10 ) ]
-        ];
-
-        $Table->setData( $data );
+        $Table->setLines( [$id_line, $initid_line, $name_line, $age_line, $level_line, $btns_line] );
 
         $AppInitDataL = new AppInitDataL( "tests" );
-        $init_list = $AppInitDataL->getTableModelObj()->paginate( 15 );
-        $init_list->withPath( \url()->full() );
-        return $Table->view( ["pages" => $init_list->links()] );
+        $init_list = $AppInitDataL->getTableModelObj()->paginate( 100 );
+        $init_list->withPath( \url()->full() ); //设置分页URL
+
+        // $list_data = $init_list->toArray();
+        // $Table->setData( $list_data['data'] );
+        // $Table->setPages( $init_list->links() );
+        $Table->setPaginator( $init_list );
+
+        return $Table->view( );
 
         $data = [];
         return \response()->json( static::jsonRes(404, null, $data) );
