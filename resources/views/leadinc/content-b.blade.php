5b0b5317d7a8abdb0acb6615e0e87089223e7da3
llseng <1300904522@qq.com>
2020-09-12 15:45:19 +0800

    style:更换面包屑导航地址

--- resources/views/leadinc/content-b.blade.php
+++ resources/views/leadinc/content-b.blade.php
@@ -2,7 +2,7 @@
 <!-- Breadcrumb-->
 <div class="breadcrumb-holder container-fluid">
 <ul class="breadcrumb">
-    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
     <!-- <li class="breadcrumb-item active">Forms</li> -->
 </ul>
 </div>
