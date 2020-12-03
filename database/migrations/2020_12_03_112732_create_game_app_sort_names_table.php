f6a6834b226c34ada2f9d2f18d2cc8829339dd9a
llseng <1300904522@qq.com>
2020-12-03 17:24:53 +0800

    feat(datebase):分类名称表

---
+++ database/migrations/2020_12_03_112732_create_game_app_sort_names_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGameAppSortNamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_app_sort_names', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger( 'sup_id' )->default(0)->comment('上级ID');
+            $table->unsignedTinyInteger( 'platform_id' )->comment('平台ID');
+            $table->unsignedTinyInteger( 'level' )->default(0)->comment('类型级别 0 组, 1 计划, 2 创意');
+            $table->unsignedBigInteger( 'sort_id' )->comment('类型ID');
+            $table->string( 'sort_name', 256 )->nullable()->comment('类型名称');
+            $table->string( 'sup_chain', 32 )->nullable()->comment('父级链');
+            $table->timestamps();
+
+            $table->index(['platform_id', 'level', 'sort_id'], 'p_level_sort');
+            $table->index(['sort_id', 'sort_name'], 'sort_id_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_app_sort_names');
+    }
+}
