147a1f6c8eae8fb40681619805b485dd51f9540e
llseng <1300904522@qq.com>
2020-12-07 19:04:13 +0800

    feat(controller):新增广告信息接口并使用

--- routes/api.php
+++ routes/api.php
@@ -57,5 +57,6 @@ Route::namespace('Api')->prefix('homeChart')->group( function () {
  * 应用日志Api路由组
  */
 Route::namespace("Api")->prefix('appLog')->group( function () {
-    Route::get( "/{app_id}/adinfo", "AppLogController@adinfo" )->where('app_id', "\w+")->name( 'applog_adinfo' );
+    Route::get( "/{app_id}/adinfo", "AppLogController@adinfo_v2" )->where('app_id', "\w+")->name( 'applog_adinfo' );
+    Route::get( "/{app_id}/adinfo_v2", "AppLogController@adinfo_v2" )->where('app_id', "\w+")->name( 'applog_adinfo_v2' );
 } );
