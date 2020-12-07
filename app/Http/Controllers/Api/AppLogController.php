e21692277fd79777992de90c4971fb82d43115b6
llseng <1300904522@qq.com>
2020-12-07 19:04:13 +0800

    feat(controller):新增广告信息接口并使用

--- app/Http/Controllers/Api/AppLogController.php
+++ app/Http/Controllers/Api/AppLogController.php
@@ -92,4 +92,45 @@ class AppLogController extends Controller
         return \response()->json( static::jsonRes( 0, null, $result, false ) );
     }
 
+    public function adinfo_v2( Request $request, $app_id ) {
+        $today = date("Y-m-d");
+        $today_stime = \date("H:i:s", time() - 600 );
+        $today_etime = \date("H:i:s");
+
+        $start_datetime = $request->input('start_datetime', $today. " ". $today_stime);
+        $end_datetime = $request->input('end_datetime', $today. " ". $today_etime);
+        $start_time = \strtotime( $start_datetime );
+        $end_time = \strtotime( $end_datetime );
+
+        $AppUsersL = new AppUsersL( $app_id );
+        $AppByteClickDataL = new AppByteClickDataL( $app_id );
+        $AppUsersM = $AppUsersL->getTableModelObj();
+        $AppByteClickDataM = $AppByteClickDataL->getTableModelObj();
+
+        $start_date = date( "Y-m-d", $start_time );
+        $start_dtime = date( "H:i:s", $start_time );
+        $end_date = date( "Y-m-d", $end_time );
+        $end_dtime = date( "H:i:s", $end_time );
+
+        $date_where = [];
+        if( $start_date == $end_date ) {
+            $date_where = [
+                [ 'create_date', '=', $start_date ],
+                [ 'create_time', '>=', $start_dtime ],
+                [ 'create_time', '<=', $end_dtime ],
+            ];
+        }else{
+            $date_where = [
+                [ 'create_date', '>=', $start_date ],
+                [ 'create_date', '<=', $end_date ],
+                [ DB::raw( "CONCAT( create_date, ' ', create_time )" ), ">=", $start_datetime ],
+                [ DB::raw( "CONCAT( create_date, ' ', create_time )" ), "<=", $end_datetime ],
+            ];
+        }
+
+        $result = $AppUsersM->select("init_id", "unique_id", "channel", "gid", "aid", "cid")->where( $date_where )->get()->toArray();
+
+        return \response()->json( static::jsonRes( 0, null, $result, false ) );
+    }
+
 }
