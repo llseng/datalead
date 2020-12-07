546c7d50965ad2807f52fafb40abc69f705015f6
llseng <1300904522@qq.com>
2020-12-07 15:34:13 +0800

    feat(database):新增用户广告冗余数据字段

---
+++ database/migrations/2020_12_05_174148_add_ad_redundancy_info_to_game_app_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdRedundancyInfoToGameAppUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('game_app_users', function (Blueprint $table) {
+            $table->unsignedBigInteger( 'cid' )->nullable()
+            ->after("channel")->comment('创意ID');
+            $table->unsignedBigInteger( 'aid' )->nullable()
+            ->after("channel")->comment('计划ID');
+            $table->unsignedBigInteger( 'gid' )->nullable()
+            ->after("channel")->comment('组ID');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('game_app_users', function (Blueprint $table) {
+            $table->dropColumn( 'aid' );
+            $table->dropColumn( 'cid' );
+            $table->dropColumn( 'gid' );
+        });
+    }
+}
