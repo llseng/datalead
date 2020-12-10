52a3863b8dbfb97d3f37f13027e196b548a96efc
llseng <1300904522@qq.com>
2020-12-10 17:05:32 +0800

    test

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -6,16 +6,19 @@ use Illuminate\Http\Request;
 
 use DB;
 use Auth;
-use App\Logic;
 use App\BaseModel;
 
 use App\Logic\LeadContent as LC;
+
+use App\Logic\AppUsers as AppUsersL;
 use App\Logic\AppInitData as AppInitDataL;
+use App\Logic\AppClickData as AppClickDataL;
+
 use App\Http\Requests\Test as TestVali;
 
 class Test extends Controller
 {
     public function index(TestVali $request) {
-        
+
     }
 }
